0b3a0a52be5e4e5c0782990538d63858ca7aab40
Kugavathanan Yohanathan <kugajpx2012@gmail.com>
2023-02-12 15:56:48 -0500

    Issue fix: contact

--- routes/index.js
+++ routes/index.js
@@ -27,7 +27,7 @@ router.get('/services', function(req, res, next) {
 });
 
 //GET contact-me page
-router.get('/contact-me', function(req, res, next) {
+router.get('/contact', function(req, res, next) {
   res.locals = { req: req };
   res.render('contact', { title: "Kugavathanan | Contact Me" });
 });
