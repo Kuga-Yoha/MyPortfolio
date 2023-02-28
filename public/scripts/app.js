724532484388f2a5b185b5f93cc9310971662f87
Kugavathanan Yohanathan <kugajpx2012@gmail.com>
2023-02-28 17:47:07 -0500

    add delete button event

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,5 +2,17 @@
     function start() {
         console.log("App Started...");       
     }
+
+
+    let deleteButton = document.querySelectorAll(".btn-delete");
+        for(button of deleteButton)
+        {
+            button.addEventListener('click',(event)=>{
+                if(!confirm("Are you sure?")){
+                    event.preventDefault();
+                    window.location.assign('/contacts/contactList');
+                }
+            })
+        }
     window.addEventListener("load", start);
 })();
