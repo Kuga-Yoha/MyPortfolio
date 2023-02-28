ec4c82223e65ff748ac0f3b26fc25b5ff33395e8
Kugavathanan Yohanathan <kugajpx2012@gmail.com>
2023-02-28 02:59:39 -0500

    add login, logout route

--- server/routes/index.js
+++ server/routes/index.js
@@ -20,14 +20,31 @@ router.get('/services', indexController.displayServicesPage);
 //GET contact-me page
 router.get('/contact', indexController.displayContactsPage);
 
-//Get Error"Message
+//Get Error Message
 router.get('/error', indexController.displayErrorPage);
 
 // router.get("", function (req, res, next) {
 //   res.render("error", { title: "Error", message: "404" });
 // });
 
+//GET contacts
 router.get('/contacts', contactsController.displayContactsPage);
 
+//GET login page
+router.get('/login', indexController.displayLoginPage);
+
+//POST login process
+router.post('/login', indexController.processLoginPage);
+
+//GET to perform user Logout
+router.get('/logout', indexController.performLogout);
+
+/*
+//GET Register for 
+router.get('/register', indexController.displayRegisterPage);
+
+//POST Router for processing registration
+router.post('/register', indexController.processRegisterPage);
+*/
 
 module.exports = router;
